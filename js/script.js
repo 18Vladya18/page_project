797019a3147eceb1ee143f8fe754fa670b1f9e27
Vladislav <solist95@mail.ru>
2022-06-03 23:38:17 +0300

    The practice of the 27th lesson is done.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ Done 2) Изменить жанр фильма, поменять "комедия
 Done 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Done 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
 
-5) Добавить нумерацию выведенных фильмов */
+Done 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
@@ -26,7 +26,8 @@ const movieDB = {
 
 const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
-      genre = poster.querySelector('.promo__genre');
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
 adv.forEach(item => {
     item.remove();
@@ -34,4 +35,16 @@ adv.forEach(item => {
 
 genre.textContent = 'драма';
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
